ba6d072c7ce2b15e6ba2fb766275f64f783483a6
Chavan Renuka <130539172+chavanrenuka22@users.noreply.github.com>
2024-01-26 16:36:34 +0530

    Add files via upload
    files added

---
+++ SpringbootStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qsp.springboot_student_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentManagementSystemApplication.class, args);
+	}
+
+}
